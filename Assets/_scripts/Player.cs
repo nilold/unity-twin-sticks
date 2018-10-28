ee21513ffd8980114f83b80c56afc9c2df2e4f5e
nilo <nilold@gmail.com>
2018-10-28 16:50:57 -0300

    started crossplatforminput

---
+++ Assets/_scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        float horizontalAxis = CrossPlatformInputManager.GetAxis("Horizontal");
+        float verticalAxis = CrossPlatformInputManager.GetAxis("Vertical");
+        Debug.Log(horizontalAxis);
+    }
+}
